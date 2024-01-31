6ee1ae6cff037d4550d3c2ca08e48fb3c9787eb6
siakagassama <siakag@gmail.com>
2024-01-30 22:10:29 -0700

    style fix

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -31,7 +31,7 @@ const Portfolio = () => {
 
     return (
 
-      <motion.main className={`essgee-page !pb-4`}
+      <motion.main className={`essgee-page !pb-[80px]`}
                      variants={pageTransitionsAnimation}
                      initial="initial"
                      animate="animate"
