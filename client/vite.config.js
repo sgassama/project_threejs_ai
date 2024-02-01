d5a1572bc462a72941b94d2805c102fa135461d7
siakagassama <siakag@gmail.com>
2024-02-01 14:42:01 -0700

    image optimizer

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import vsharp from 'vite-plugin-vsharp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    vsharp(),
   ],
   resolve: {
     alias: {
